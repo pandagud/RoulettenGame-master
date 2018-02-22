29c097d7da04e79b3ac7bb15ab1f57320d29ab17
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-02-22 11:57:03 +0100

    MAde some AWESOME changes!

---
+++ RoulettenGame/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace RoulettenGame
+{
+    [TestFixture]
+    class TestClass
+    {
+        [SetUp]
+        void Startup()
+        {
+
+        }
+        [Test]
+        public void Some_Test()
+        {
+
+        }
+    }
+}
